40f73ae59a7dbe46c93f852b1c511407e707eaf4
Dmitry757 <43566694+Dmitry757@users.noreply.github.com>
2022-10-28 23:58:01 +0300

    change JPARepository to CRUDRepository ))

--- src/main/java/org/dng/_2022_10_26_springapi_ex1/Controllers/AlbumController.java
+++ src/main/java/org/dng/_2022_10_26_springapi_ex1/Controllers/AlbumController.java
@@ -1,12 +1,9 @@
 package org.dng._2022_10_26_springapi_ex1.Controllers;
 
-
-
 import org.dng._2022_10_26_springapi_ex1.Model.Album;
 import org.dng._2022_10_26_springapi_ex1.Service.AlbumService;
 import org.springframework.web.bind.annotation.*;
 
-import javax.websocket.server.PathParam;
 import java.util.List;
 
 @RestController
--- src/main/java/org/dng/_2022_10_26_springapi_ex1/DAO/AlbumRepository.java
+++ src/main/java/org/dng/_2022_10_26_springapi_ex1/DAO/AlbumRepository.java
@@ -1,7 +1,8 @@
 package org.dng._2022_10_26_springapi_ex1.DAO;
 
 import org.dng._2022_10_26_springapi_ex1.Model.Album;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 
-public interface AlbumRepository extends JpaRepository<Album, Long> {
+//public interface AlbumRepository extends JpaRepository<Album, Long> {
+public interface AlbumRepository extends CrudRepository<Album, Long> {
 }
